fd82cb0a2c8d0e4e62cc6633e8f638b1e834f495
25kopek <dellinner@gmail.com>
2020-06-01 16:49:33 +0700

    pravki ended)

--- js/script.js
+++ js/script.js
@@ -81,39 +81,6 @@ mobileMenuLinks.forEach(function (e) {
 });
 
 
-// // quiz
-//
-// let buttonFirst = document.getElementsByClassName('quiz__button_first')[0];
-// let buttonSecond = document.getElementsByClassName('quiz__button_second')[0];
-// let buttonThird = document.getElementsByClassName('quiz__button_third')[0];
-// let buttonFourth = document.getElementsByClassName('quiz__button_fourth')[0];
-//
-// let quizFirst = document.getElementsByClassName('quiz__question_first')[0];
-// let quizSecond = document.getElementsByClassName('quiz__question_second')[0];
-// let quizThird = document.getElementsByClassName('quiz__question_third')[0];
-// let quizFourth = document.getElementsByClassName('quiz__question_fourth')[0];
-// let quizFifth = document.getElementsByClassName('quiz__question_fifth')[0];
-//
-// buttonFirst.addEventListener('click', function () {
-//     quizFirst.style.display = 'none';
-//     quizSecond.style.display = 'block';
-// });
-//
-// buttonSecond.addEventListener('click', function () {
-//     quizSecond.style.display = 'none';
-//     quizThird.style.display = 'block';
-// });
-//
-// buttonThird.addEventListener('click', function () {
-//     quizThird.style.display = 'none';
-//     quizFourth.style.display = 'block';
-// });
-//
-// buttonFourth.addEventListener('click', function () {
-//     quizFourth.style.display = 'none';
-//     quizFifth.style.display = 'block';
-// });
-
 // forms to PHPMailer
 // Отправка данных на сервер - квиз
 
